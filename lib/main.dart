6af495d9d2abb6da2ca51b899294db54d2cb14f3
abdelaatife <abdoabdo2002104h@gmail.com>
2022-07-13 02:55:00 +0100

    phone authe

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
-import 'package:app/home.dart';
+import 'package:app/page_home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:splash_screen_view/SplashScreenView.dart';
 
-void main() {
-  runApp(MaterialApp(
+import 'page_phone_auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Firebase Phone Auth',
       debugShowCheckedModeBanner: false,
-      home: SplashScreenView(
-        navigateRoute: const home(),
-        duration: 4000,
-        imageSize: 500,
-        text: "Welcome to ECG T-shirt",
-        imageSrc:
-            "assets/xiaomi-Mijia-Sports-Cardiogram-t-shirt-ecg-tricko-5-removebg-preview.png",
-        backgroundColor: Colors.white,
-        pageRouteTransition: PageRouteTransition.SlideTransition,
-        textStyle: const TextStyle(
-          fontSize: 20,
-          color: Color.fromARGB(255, 0, 0, 0),
-        ),
-      )));
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: PhoneAuthPage(),
+      // NOTE: This will check if the user is already signed in
+      home: StreamBuilder<User?>(
+          stream: firebaseAuth.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return HomePage();
+            }
+            return PhoneAuthPage();
+          }),
+    );
+  }
 }
